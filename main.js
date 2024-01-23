d30c7a45a39ea0f9375cc64f0874504c878e3219
Sevinch <94165905+SevinchHamidova@users.noreply.github.com>
2024-01-23 10:53:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+// vazifa 1
+// let count = 0;
+// const arr = ["ona", "Ali", "Hi"];
+// for (const iterator of arr) {
+//   count += iterator.length
+// }
+// console.log(count);
+
+
+// vazifa 2
+// let word = prompt("word ");
+// for (words  of word) {
+//     if (word.toLowerCase() === "hi" || word.toLowerCase() === "hello"){
+//         console.log("assalomu aleykum");
+//     }else{
+//         console.log("szni gapizda hi yoki hello yoq");
+//     }
+// }
+
+
+// vazifa 3
+// let word = ["ali", "olma", "ozbekiston"];
+// let uzun = "";
+// for (engUzun of word) {
+//     if (engUzun.length > uzun.length) {
+//         uzun = engUzun;
+//       }
+// }
+// console.log(uzun);
+
+
+// Vazifa 4
+// const arr = [ 1, 2, 3, 4, 66, 99];
+// let boshidagiSon = arr[0];
+// let oxirgiSon = arr[arr.length - 1];
+// let natija = boshidagiSon+oxirgiSon;
+// console.log(natija);
+
+
+// vazifa 5
+// const word = prompt("Enter word");
+// const arrLetters = word.split("");
+// let count = 0;
+// for (const iterator of arrLetters) {
+//   if(iterator == iterator.toUpperCase()) {
+//     count++
+//   }
+// }
+// console.log(count);
+// HOMEWORK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// vazifa 6
+// const word = prompt("enter word");
+// let uzunlik = word.length;
+// let harf = word.charAt(uzunlik - 1);
+// let harfSoni = 0;
+
+// for ( harfi of word) {
+//   if (harfi == harf) {
+//       harfSoni++;
+//   }
+// }
+
+// console.log(`kiritilgan ${word} so'zida ${harfSoni} ta o'xshash har bor`);
+
+// vazifa 7 
+// const arr = [7, 9, 43, 8];
+// let yigindi = 0;
+// for ( son of arr) {
+//   yigindi += son;
+// }
+// console.log(`arraydagi sonlar yig'indisi ${yigindi}`);
+
+// vazifa 8
+// const arr = ["olma", "nok", "shaftoli"];
+// let arrOne = arr[0];
+// let boshHarf = arrOne.slice(1, 1).toUpperCase();
+// let oxirgiHarf = arrOne.slice(-1).toUpperCase();
+// let newArr = boshHarf + oxirgiHarf;
+// arr[0] += newArr;
+// console.log(arr);
+
